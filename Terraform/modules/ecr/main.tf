cde7c3d50eebc4cfc48f244834059e757b71c16f
Daniel Rojas <notdanielrojas@gmail.com>
2025-01-05 17:38:29 -0300

    testing new config

--- Terraform/modules/ecr/main.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecr_repository" "ecr-prueba-final" {
-  name = var.repository_name
-}
--- Terraform/modules/ecr/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "repository_url" {
-  value = aws_ecr_repository.ecr-prueba-final.repository_url
-  description = "URL del repositorio ECR"
-}
