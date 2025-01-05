6cc93556083947328f96e729845546752174c516
Daniel Rojas <notdanielrojas@gmail.com>
2025-01-05 18:22:25 -0300

    Final push

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -8,31 +8,52 @@ resource "aws_instance" "ec2-prueba-final" {
 
   user_data = <<-EOF
     #!/bin/bash
-    sudo apt update
-    sudo apt install -y git curl nodejs npm
+    echo "Instalando Nginx y configurando servidor para archivos estáticos..."
 
-    # Clonar el repositorio
-    git clone https://github.com/notdanielrojas/prueba-final-devops.git /home/ubuntu/app
+    # Instalar Nginx usando amazon-linux-extras
+    sudo amazon-linux-extras enable nginx1
+    sudo yum install -y nginx
+    sudo systemctl enable nginx
+    sudo systemctl start nginx
 
-    # Instalar dependencias
-    cd /home/ubuntu/app
-    npm install
+    # Crear directorio para la aplicación si no existe
+    sudo mkdir -p /var/www/landingpage
 
-    # Instalar PM2 y ejecutar la app
-    sudo npm install pm2 -g
-    pm2 start npm --name "landingpage-yossequiropractica" -- start
-    pm2 save
+    # Copiar archivos estáticos a la carpeta web
+    sudo cp -r /home/ec2-user/app/App/* /var/www/landingpage/
+
+    # Crear la configuración de Nginx
+    sudo mkdir -p /etc/nginx/conf.d
+    sudo tee /etc/nginx/conf.d/landingpage.conf > /dev/null <<EOL
+    server {
+        listen 80;
+
+        # Raíz de la aplicación
+        root /var/www/landingpage;
+        index index.html;
+
+        # Configuración para servir archivos estáticos
+        location / {
+            try_files \$uri \$uri/ =404;
+        }
+
+        # Manejo de errores
+        error_page 404 /404.html;
+    }
+    EOL
+
+    # Recargar configuración de Nginx
+    sudo nginx -s reload
   EOF
 
   tags = {
     Name = var.instance_name
   }
 
-
   # Conexión SSH
   connection {
     type = "ssh"
-    user = "notdanielrojas"
+    user = "ec2-user"
     host = self.public_ip
     private_key = file("\\\\wsl.localhost\\Ubuntu\\media\\keys\\keypair-ssh.pem")
   }
